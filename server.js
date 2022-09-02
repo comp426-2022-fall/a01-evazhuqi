263647bd6291dda50faeeae46f51516e63427846
evazhuqi <78308351+evazhuqi@users.noreply.github.com>
2022-09-01 22:17:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,14 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
   res.setHeader("Content-Type", "text/html");
   res.end(data);
 });
+  
+  
+// Start the `server` const listening on the port defined by argument in your `port` const. 
+// Put the exact message `Server listening on port ${port}` on the console log. 
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+// That's it! You're all done!
 });
 
 
@@ -50,11 +58,3 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 
 
 
-
-
-// Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
-// That's it! You're all done!
